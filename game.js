c93b4211cd0894f2d9a4f0900a6cc2ed46fd2eee
dem08656775 <70008686+dem08656775@users.noreply.github.com>
2021-07-31 08:19:20 +0900

    Add files via upload

--- game.js
+++ game.js
@@ -1,13 +1,27 @@
 var val = 0;
 
 var player = {
-  money:　new Decimal(0)
+  money: new Decimal(0)
+
 };
 
 function update(){
   player.money = player.money.add(1)
-  document.getElementById("coinamount").textContent = player.money
+  document.getElementById("coinamount").textContent = 'ポイント: '+player.money
   setTimeout(update, 1000)
 }
 
+function save(){
+  localStorage.setItem("playerStored", JSON.stringify(player));
+}
+
+function load() {
+	$.extend(true, player, JSON.parse(localStorage.getItem("playerStored")));
+  player.money = new Decimal(player.money)
+}
+
+load()
+
 setTimeout(update, 1000)
+
+setInterval(save,2000)
