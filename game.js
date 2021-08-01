786ecc9802aac5fc57e8af23200a7a28649071d2
dem08656775 <70008686+dem08656775@users.noreply.github.com>
2021-08-01 23:19:35 +0900

    Add files via upload

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ const tweetbutton = document.getElementById("tweet");
 console.log(tweetbutton);
 var val = 0;
 var version = 1;
+var tickspeed = 1000;
 
 var firstplayer = {
   money: new Decimal(1),
@@ -11,9 +12,11 @@ var firstplayer = {
   generator2: new Decimal(0),
   generator2bought: new Decimal(0),
   generator2cost: new Decimal('1e4'),
+  generator2mode: 1,
   generator3: new Decimal(0),
   generator3bought: new Decimal(0),
   generator3cost: new Decimal('1e9'),
+  generator3mode: 2,
   generator4: new Decimal(0),
   generator4bought: new Decimal(0),
   //generator4cost: 工事中,
@@ -30,32 +33,50 @@ var firstplayer = {
   generator8bought: new Decimal(0),
   //generator8cost: 工事中,
 
+  accelerator1: new Decimal(0),
+  accelerator1bought: new Decimal(0),
+  accelerator1cost: new Decimal(10),
+
+  tickspeed: 1000,
+
   saveversion: version
 
 }
+
 var player = $.extend(true,{},firstplayer)
 
 function update(){
   player.money = player.money.add(player.generator1)
-  player.generator1 = player.generator1.add(player.generator2)
-  player.generator2 = player.generator2.add(player.generator3)
+  if(player.generator2mode == 0)player.money = player.money.add(player.generator2.mul(1e4))
+  if(player.generator2mode == 1)player.generator1 = player.generator1.add(player.generator2)
+  if(player.generator3mode == 0)player.money = player.money.add(player.generator3.mul(1e9))
+  if(player.generator3mode == 1)player.generator1 = player.generator1.add(player.generator3.mul(1e4))
+  if(player.generator3mode == 2)player.generator2 = player.generator2.add(player.generator3)
   player.generator3 = player.generator3.add(player.generator4)
   player.generator4 = player.generator4.add(player.generator5)
   player.generator5 = player.generator5.add(player.generator6)
   player.generator6 = player.generator6.add(player.generator7)
   player.generator7 = player.generator7.add(player.generator8)
+  tickspeed = 1000 / player.accelerator1.add(10).log10()
+  console.log(tickspeed)
   updatetext()
-  setTimeout(update, 1000)
+  setTimeout(update, tickspeed)
 }
 
 function updatetext(){
   $("#coinamount").text('ポイント: '+ player.money.toExponential(3))
+  $("#tickspeed").text('間隙: ' + Math.round(tickspeed) + '毛秒')
   $("#generator1").text('発生器1: ' + player.generator1)
   $("#generator2").text('発生器2: ' + player.generator2)
   $("#generator3").text('発生器3: ' + player.generator3)
+  $("#accelerator1").text('時間加速器1: ' + player.accelerator1)
   $("#button1").text('購入　コスト: ' + player.generator1cost.toExponential(1))
   $("#button2").text('購入　コスト: ' + player.generator2cost.toExponential(1))
   $("#button3").text('購入　コスト: ' + player.generator3cost.toExponential(1))
+  $("#abutton1").text('購入　コスト: ' + player.accelerator1cost.toExponential(1))
+  $("#generator2mode").text('モード: '+player.generator2mode)
+  $("#generator3mode").text('モード: '+player.generator3mode)
+
   if (!tweetbutton.firstChild){
     const anchor = document.createElement('a');
     anchor.className = 'twitter-hashtag-button';
@@ -87,6 +108,7 @@ function load() {
   player.generator2 = new Decimal(player.generator2)
   player.generator2bought =  new Decimal(player.generator2bought)
   player.generator2cost = new Decimal(player.generator2cost)
+  player.generator2mode = parseInt(player.generator2mode)
   player.generator3 = new Decimal(player.generator3)
   player.generator3bought =  new Decimal(player.generator3bought)
   player.generator3cost = new Decimal(player.generator3cost)
@@ -95,9 +117,9 @@ function load() {
   player.generator6 = new Decimal(player.generator6)
   player.generator7 = new Decimal(player.generator7)
   player.generator8 = new Decimal(player.generator8)
-  if(player.saveversion<1){
-    calcbought()
-  }
+  player.accelerator1 = new Decimal(player.accelerator1)
+  player.accelerator1bought = new Decimal(player.accelerator1bought)
+  player.accelerator1cost = new Decimal(10).pow(player.accelerator1bought.add(1).mul(player.accelerator1bought.add(2)).div(2))
   player.saveversion = version
 }
 
@@ -133,6 +155,30 @@ $("#button3").on('click',function(){
   }
 })
 
+$("#abutton1").on('click',function(){
+  if(player.money.greaterThanOrEqualTo(player.accelerator1cost)){
+    player.money = player.money.sub(player.accelerator1cost)
+    player.accelerator1 = player.accelerator1.add(1)
+    player.accelerator1bought = player.accelerator1bought.add(1)
+    player.accelerator1cost = player.accelerator1cost.mul(player.accelerator1bought.add(1).pow_base(10))
+    updatetext()
+  }
+})
+
+$("#modebutton2").on('click',function(){
+  player.generator2mode += 1
+  if(player.generator2mode == 2) player.generator2mode = 0
+  updatetext()
+})
+
+$("#modebutton3").on('click',function(){
+  player.generator3mode += 1
+  if(player.generator3mode == 3) player.generator3mode = 0
+  updatetext()
+})
+
+
+
 
 $("#resetbutton").on('click',function(){
   if(confirm('リセット？')){
@@ -141,6 +187,6 @@ $("#resetbutton").on('click',function(){
   }
 })
 
-setTimeout(update, 1000)
+setTimeout(update, tickspeed)
 
 setInterval(save,2000)
