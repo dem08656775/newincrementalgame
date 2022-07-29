d52ae179d212b4fded6ea7329467feb00957f672
dem08656775 <70008686+dem08656775@users.noreply.github.com>
2022-07-29 18:26:22 +0900

    Add files via upload

--- game.js
+++ game.js
@@ -678,7 +678,8 @@ Vue.createApp({
       //if(d.getMonth()==0&&d.getDate()<=7)camp = camp + 1//新年キャンペーン
       //if(d.getMonth()==1&&8<=d.getDate()&&d.getDate()<=14)camp = camp + 1//バレンタインキャンペーン
       //if((d.getMonth()==1&&25<=d.getDate()) || ((d.getMonth()==2&&d.getDate()<=3)))camp = camp + 1//桃の節句キャンペーン
-      if(camp>4)camp=4
+      if((d.getMonth()==6&&27<=d.getDate()) || ((d.getMonth()==7&&d.getDate()<27)))camp = camp + 2//1周年キャンペーン
+      if(camp>6)camp=6
       mult = mult.mul(1 + 4 * camp)
 
       this.commonmult = mult
